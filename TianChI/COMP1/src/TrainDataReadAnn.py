4ca7d0b7d5073dcd1189596fc7625b45dbb1f41d
viewv <zxn@zxnnet.top>
2018-10-23 14:07:49 +0800

    finish read ann

--- TianChI/COMP1/src/TrainDataReadAnn.py
+++ TianChI/COMP1/src/TrainDataReadAnn.py
@@ -2,5 +2,32 @@ import os
 import sqlite3
 
 
+def lineProcess(line):
+    line = line.split()
+    classes = line[1]
+    #!['T14', 'Disease', '10415', '10416;10417', '10421', '2', '型糖尿病']
+    temp = line[3]
+    temp = temp.split(';')
+    if len(temp) == 1:
+        illness = ''.join(line[4:])
+        return [classes, illness]
+    else:
+        illness = ''.join(line[5:])
+        return [classes, illness]
+
+
 def readAnn(path):
-    pass
+    AllFiles = [x for x in os.listdir('.') if os.path.isfile(
+        x) and os.path.splitext(x)[1] == '.ann']
+    Database = sqlite3.connect('aldata.db')
+    Cursor = Database.cursor()
+    Cursor.execute(
+        'create table ANNALLWORD (id integer primary key autoincrement, ill text,class text)')
+    for x in AllFiles:
+        with open(path + '/' + x, 'r') as f:
+            for line in f.readlines():
+                wordlist = lineProcess(line)
+                classes = wordlist[0]
+                illness = wordlist[1]
+                Cursor.execute(
+                    'insert or ignore into ANNALLWORD (ill, class) values (\'%s\', \'%s\')' % (illness, classes))
