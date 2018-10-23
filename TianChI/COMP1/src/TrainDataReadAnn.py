3f2083e480f128bc4ad5c6ece7bb6ff0b19981dc
viewv <zxn@zxnnet.top>
2018-10-23 15:33:56 +0800

    successful

--- TianChI/COMP1/src/TrainDataReadAnn.py
+++ TianChI/COMP1/src/TrainDataReadAnn.py
@@ -18,26 +18,23 @@ def lineProcess(line):
 
 def readAnn(path):
     #! input path : type string, whith contain .ann files
-    typeofillness = 0
     databasepath = "/home/viewv/Tianchi_RuiJInComp/TianChI/COMP1/src/alldata.db"
     AllFiles = [x for x in os.listdir(path) if os.path.isfile(
         path+'/'+x) and os.path.splitext(path+'/'+x)[1] == '.ann']
     Database = sqlite3.connect(databasepath)
     Cursor = Database.cursor()
     Cursor.execute(
-        'create table ANNALLWORD (id integer primary key autoincrement, ill text,class text)')
+        'create table ANNALLWORD (id integer primary key autoincrement, ill text unique,class text)')
     for x in AllFiles:
         with open(path + '/' + x, 'r') as f:
             for line in f.readlines():
                 wordlist = lineProcess(line)
-                typeofillness += 1
                 classes = wordlist[0]
                 illness = wordlist[1]
                 order = "insert or ignore into ANNALLWORD (ill, class) values (" + \
                     '\"' + illness + '\"' + ',' + '\"' + classes + '\"' + ')'
-                print(order)
                 Cursor.execute("%s" % (order))
     Cursor.close()
     Database.commit()
     Database.close()
-    return "Read Train.ann data successfully! All type of ill: " + str(typeofillness)
+    return "Read Train.ann data successfully! All type of ill: "
